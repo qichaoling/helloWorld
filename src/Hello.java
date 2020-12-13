cd289b671df93284b9f6be82da3d7e077c2297be
qcl <2058274655@qq.com>
2020-12-13 09:59:31 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,6 @@
+/**
+ * @author qcl
+ * @date 2020/12/11-16:56
+ */
+public class Hello {
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+//aaaa
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        ArrayList<String> list = new ArrayList<>();
+    }
+
+    public static void a(){
+
+    }
+
+}
